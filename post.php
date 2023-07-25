a5ceb4a4517a9acb08fdd4ef1c83399a367b7f18
JoaoAlves <2003jba@gmail.com>
2023-07-25 16:33:36 +0100

    problema commentario resolvido

--- post.php
+++ post.php
@@ -59,107 +59,144 @@ include "includes/navigation.php";
 
 
 
-                 <?php    }  ?>
+                <?php } 
 
 
-                 <!-- Blog Comments -->
 
-                 <?php
-                if(isset($_POST['create_comment'])){
 
-                $the_post_id = $_GET['p_id'];
 
-                $comment_author = $_POST['comment_author'];
-                $comment_email = $_POST['comment_email'];
-                $comment_content = $_POST['comment_content'];
 
-               $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) "; 
 
-               $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved',now())";
-
-               $create_comment_query = mysqli_query($connection,$query);
-
-               if(!$create_comment_query){
-                die('QUERY FAILED' . mysqli_error($connection));    
-               }
-
-                }
 ?>
 
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form action="" method="post" role="form">
 
-                    <div class="form-group">
-                        <label for="Author">Author</label>
-                        <input type="text" name="comment_author" class="form-control" name="comment_author">
-                        </div>
+<!-- Blog Comments -->
 
-                        <div class="form-group">
-                        <label for="Author">Email</label>
-                        <input type="email" name="comment_email" class="form-control" name="comment_email">
-                        </div>
+<?php 
 
-                        <div class="form-group">
-                        <label for="Author">Your Comments</label>
-                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+    if(isset($_POST['create_comment'])) {
 
-                <hr>
+        $the_post_id = $_GET['p_id'];
+        $comment_author = $_POST['comment_author'];
+        $comment_email = $_POST['comment_email'];
+        $comment_content = $_POST['comment_content'];
+
+
+        if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
+
+
+            $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+$query .= "VALUES ($the_post_id ,'{$comment_author}', '{$comment_email}', '{$comment_content }', 'approve', now())"; 
+
+            $create_comment_query = mysqli_query($connection, $query);
+
+            if (!$create_comment_query) {
+                die('QUERY FAILED' . mysqli_error($connection));
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+
+
+?> 
+
 
                 <!-- Posted Comments -->
 
-                <?php
-            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-            $query .= "AND comment_status = 'approved' ";
-            $query .= "ORDER BY comment_id DESC";
+
+
+        <!-- Comments Form -->
+        <div class="well">
+
+
+
+            <h4>Leave a Comment:</h4>
+            <form action="#" method="post" role="form">
+
+                <div class="form-group">
+                    <label for="Author">Author</label>
+                    <input type="text" name="comment_author" class="form-control" name="comment_author">
+                </div>
+
+                <div class="form-group">
+                    <label for="Author">Email</label>
+                    <input type="email" name="comment_email" class="form-control" name="comment_email">
+                </div>
+
+                <div class="form-group">
+                    <label for="comment">Your Comment</label>
+                    <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+        <hr>
+                
+                 <?php 
+
+
+$query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+$query .= "AND comment_status = 'approve' "; 
+$query .= "ORDER BY comment_id DESC ";
             $select_comment_query = mysqli_query($connection, $query);
-            if(!$select_comment_query){
-                die('QUERY Failed' . mysqli_error($connection));
-            }
-            while ($row = mysqli_fetch_array($select_comment_query)){
-                $comment_date = $row['comment_date'];
-                $comment_content = $row['comment_content'];
-                $comment_author = $row['comment_author'];
-            
+            if(!$select_comment_query) {
 
-?>
-
-
- <!-- Comment -->
- <div class="media">
+                die('Query Failed' . mysqli_error($connection));
+             }
+            while ($row = mysqli_fetch_array($select_comment_query)) {
+            $comment_date   = $row['comment_date']; 
+            $comment_content= $row['comment_content'];
+            $comment_author = $row['comment_author'];
+                
+                ?>
+                
+                
+                           <!-- Comment -->
+                <div class="media">
+                     
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading"><?php echo  $comment_author; ?>
-                            <small><?php echo $comment_date; ?></small>
+                        <h4 class="media-heading"><?php echo $comment_author;   ?>
+                            <small><?php echo $comment_date;   ?></small>
                         </h4>
-                        <?php echo $comment_content; ?>
+                        
+                        <?php echo $comment_content;   ?>
+ 
                     </div>
                 </div>
+     
+                
+  
 
+           <?php }  ?>
+           
+  
 
-
-
-<?php } ?>
-
-             
             </div>
+            
+              
 
             <!-- Blog Sidebar Widgets Column -->
-           <?php include "includes/sidebar.php"; ?>
-
+            
+            
+            <?php include "includes/sidebar.php";?>
+             
 
         </div>
         <!-- /.row -->
 
         <hr>
 
-       <?php 
-       include "includes/footer.php";
-       ?>
+   
+
+<?php include "includes/footer.php";?>
