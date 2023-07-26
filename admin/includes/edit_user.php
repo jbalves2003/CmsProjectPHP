e2c943321888880b0cdc305df6fad180ef26d876
JoaoAlves <2003jba@gmail.com>
2023-07-26 11:31:31 +0100

    add users

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -75,6 +75,28 @@
 <input  value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
 </div>
 
+
+
+
+<div class="form-group">
+ <select name="user_role"id="">
+
+ <?php
+        $query = "SELECT * FROM users ";
+        $select_users = mysqli_query($connection, $query);
+
+        comfirmQuery($select_users);
+
+        while($row = mysqli_fetch_assoc($select_users)){
+       $user_id = $row['user_id'];
+       $user_role = $row['user_role'];
+
+       echo "<option value='$user_id'>{$user_role}</option>";
+        }
+        ?>
+ </select>      
+</div>
+
 <div class="form-group">
  <select name="post_category"id="post_category">
 
