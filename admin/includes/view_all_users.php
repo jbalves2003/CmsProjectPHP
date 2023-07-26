d9bab6dff09526563c73954c27c309087796aadd
JoaoAlves <2003jba@gmail.com>
2023-07-26 10:08:33 +0100

    Display All Users

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -2,15 +2,12 @@
                             <thead>
                                 <tr>
                                     <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Comment</th>
+                                    <th>Username</th>
+                                    <th>FirstName</th>
+                                    <th>LastName</th>
                                     <th>Email</th>
-                                    <th>Status</th>
-                                    <th>In Response to</th>
-                                    <th>Date</th>
-                                    <th>Approve</th>
-                                    <th>Unapprove</th>
-                                    <th>Delete</th>
+                                    <th>Role</th>
+                                    
 
                                 </tr>
                             </thead>
@@ -18,23 +15,24 @@
                             <tbody>
                 <?php 
                                 
-                                $query = "SELECT * FROM comments";
-                                $select_comments = mysqli_query($connection, $query);
+                                $query = "SELECT * FROM users";
+                                $select_users = mysqli_query($connection, $query);
                                 
-                                   while($row = mysqli_fetch_assoc($select_comments)){
-                                       $comment_id = $row['comment_id'];
-                                       $comment_post_id = $row['comment_post_id'];
-                                       $comment_author = $row['comment_author'];
-                                       $comment_content = $row['comment_content'];
-                                       $comment_email = $row['comment_email'];
-                                       $comment_status = $row['comment_status'];
-                                       $comment_date = $row['comment_date'];
+                                   while($row = mysqli_fetch_assoc($select_users)){
+                                       $user_id = $row['user_id'];
+                                       $username = $row['username'];
+                                       $user_password = $row['user_password'];
+                                       $user_firstname = $row['user_firstname'];
+                                       $user_lastname = $row['user_lastname'];
+                                       $user_email = $row['user_email'];
+                                       $user_image = $row['user_image'];
+                                       $user_role = $row['user_role'];
                                        
 
                                        echo "<tr>";
-                                       echo "<td>$comment_id</td>";
-                                       echo "<td>$comment_author</td>";
-                                       echo "<td>$comment_content</td>";
+                                       echo "<td>$user_id</td>";
+                                       echo "<td>$username</td>";
+                                       echo "<td>$user_firstname</td>";
 
                                 /*       $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
                                        $select_categories_id = mysqli_query($connection, $query);
@@ -48,9 +46,11 @@
                                     }
 
                                     */
-                                       echo "<td>$comment_email</td>";
-                                       echo "<td>$comment_status</td>";
+                                       echo "<td>$user_lastname</td>";
+                                       echo "<td>$user_email</td>";
+                                       echo "<td>$user_role</td>";
 
+                                    /*
                                        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
                                        $select_post_id_query = mysqli_query($connection,$query);
                                        while($row = mysqli_fetch_assoc($select_post_id_query)){
@@ -59,12 +59,11 @@
                                         echo "<td><a href='../posts.php?p_id=$post_id'>$post_title</a></td>";
 
                                        }
+                                    */
 
-
-                                       echo "<td>$comment_date</td>";
-                                       echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
-                                       echo "<td><a href='comments.php?unapprove=$comment_id'>UnApprove</a></td>";
-                                       echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
+                                       echo "<td><a href='comments.php?approve='>Approve</a></td>";
+                                       echo "<td><a href='comments.php?unapprove='>UnApprove</a></td>";
+                                       echo "<td><a href='comments.php?delete='>Delete</a></td>";
                                        echo "</tr>";
 
                                    }
