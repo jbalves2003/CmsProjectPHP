4d3daf69f6091779c920b542027134d478aa8e64
JoaoAlves <2003jba@gmail.com>
2023-07-26 09:49:24 +0100

    Create User database and associate code

--- index.php
+++ index.php
@@ -5,74 +5,83 @@
 include "includes/header.php";
 ?>
 
-    <!-- Navigation -->
-    
-    <?php  
+<!-- Navigation -->
+
+<?php  
 include "includes/navigation.php";
 ?>
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-        <div class="row">
+<div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-                <?php 
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
+        <?php 
 
-                    $query= "SELECT * FROM posts";
-                    $select_all_posts_query = mysqli_query($connection, $query);
+            $query= "SELECT * FROM posts ";
+            $select_all_posts_query = mysqli_query($connection, $query);
 
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                       $post_id =  $row['post_id'];
-                       $post_title =  $row['post_title'];
-                       $post_author =  $row['post_author'];
-                       $post_date =  $row['post_date'];
-                       $post_image =  $row['post_image'];
-                       $post_content = substr($row['post_content'],0,100);
-                   
-                       ?>
+            while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                $post_id =  $row['post_id'];
+                $post_title =  $row['post_title'];
+                $post_author =  $row['post_author'];
+                $post_date =  $row['post_date'];
+                $post_image =  $row['post_image'];
+                $post_content = substr($row['post_content'],0,100);
+                $post_status =  $row['post_status'];
 
-                    <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                if($post_status !== 'published' && $post_status !== 'Published'){
+                echo "<h1> No POST SORRY </h1>";
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date  ?></p>
-                <hr>
+                }else{
 
-                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
-                <hr>
-                <p><?php echo $post_content  ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                
 
-                <hr>
+            
+                ?>
 
+            <h1 class="page-header">
+            Page Heading
+            <small>Secondary Text</small>
+        </h1>
 
+        <!-- First Blog Post -->
+        <h2>
+            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+        </h2>
+        <p class="lead">
+            by <a href="index.php"><?php echo $post_author ?></a>
+        </p>
+        <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date  ?></p>
+        <hr>
 
-
-                 <?php    }  ?>
-
-              
-
-
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-           <?php include "includes/sidebar.php"; ?>
-
-
-        </div>
-        <!-- /.row -->
+        <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+        <hr>
+        <p><?php echo $post_content  ?></p>
+        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
         <hr>
 
-       <?php 
-       include "includes/footer.php";
-       ?>
+
+
+
+            <?php    }  } ?>
+
+        
+
+
+    </div>
+
+    <!-- Blog Sidebar Widgets Column -->
+    <?php include "includes/sidebar.php"; ?>
+
+
+</div>
+<!-- /.row -->
+
+<hr>
+
+<?php 
+include "includes/footer.php";
+?>
